5659e33ebef505399dcb18fdba786b88c868cb7b
Rishikesh Dhanavde <rishidhanavde@gmail.com>
2026-04-20 16:00:22 +0530

    Dicee Flutter App
    Completed the app. Learned Stateful and Stateless widgets

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,7 +7,10 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text(
+            'Dicee',
+            style: TextStyle(color: Colors.white),
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,7 +19,16 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -23,18 +36,56 @@ class DicePage extends StatelessWidget {
         children: [
           Expanded(
             child: TextButton(
-              child: Image.asset("images/dice1.png"),
-              onPressed: null,
+              child: Image.asset("images/dice$leftDiceNumber.png"),
+              onPressed: () => changeDiceFace(),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset("images/dice1.png"),
+            child: TextButton(
+              child: Image.asset("images/dice$rightDiceNumber.png"),
+              onPressed: () => changeDiceFace(),
             ),
           ),
         ],
       ),
     );
   }
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
 }
+
+//This was the Stateless widget used to learn things before changing to stateful widget
+
+// class DicePage extends StatelessWidget {
+//   int leftDiceNumber = 1;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Row(
+//         children: [
+//           Expanded(
+//             child: TextButton(
+//               child: Image.asset("images/dice$leftDiceNumber.png"),
+//               onPressed: () {
+//                 print("Left Button pressed");
+//               },
+//             ),
+//           ),
+//           Expanded(
+//             child: TextButton(
+//               child: Image.asset("images/dice1.png"),
+//               onPressed: () {
+//                 print("Right Button Pressed");
+//               },
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
